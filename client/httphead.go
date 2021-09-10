71445b4d2f40abb196caa5e94318acdd70b613ed
toniz <toniz7960@gmail.com>
2021-09-10 19:12:02 +0800

    add

--- client/httphead.go
+++ client/httphead.go
@@ -6,17 +6,13 @@ import (
     "context"
     "time"
 
-    //"go.opentelemetry.io/otel/trace"
-    . "github.com/bbthis/frame"
-    . "github.com/bbthis/codes"
-    . "github.com/bbthis/gosuit/cipher"
-    tracelog "github.com/bbthis/gosuit/trace"
     "go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+    tracelog "github.com/toniz/otel"
 )
 
 func main() {
-    err := tracelog.SetGrpcExport(context.TODO(), "trace_ali_bbthis.json.enc", KeyConfuse(FRAME_ConfigKey, "secret", 32), "OrderService", "v0.3.20")
-    if err != OK {
+    err := tracelog.SetGrpcExport(context.TODO(), "trace_ali_bbthis.json", "OrderService", "v0.3.20")
+    if err != nil {
         panic(err)
     }
 
